08ddf929bec3b5fd99f6154b1a10a57d4322e704
Shivanshu Bansal <sdev.shivanshu@gmail.com>
2023-04-21 04:55:42 -0700

    add sql support to main.py
    tested with self data and incorrect query
    everything passing!

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import discord
 from discord.ext import commands
 import os
+import sqlite3
 
 TOKEN = os.environ["TOKEN"]
-bot = commands.Bot('^', token = TOKEN)
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='^', intents=intents, token=TOKEN)
+
+conn = sqlite3.connect('rescind_bot.db')
+cursor = conn.cursor()
 
 async def check_for_rescind(message):
     # check if the message contains rescind
@@ -19,9 +25,20 @@ async def count(ctx, user: commands.MemberConverter):
     # Get the user's 'rescind' counter from a database or file
     # Send a message to the channel with the user's counter
     # await ctx.send(f"{user.mention}, your rescind count is {counter}.")
+    pass
 
 # listen for incoming messages
 @bot.event
 async def on_message(message):
     await check_for_rescind(message)
     await bot.process_commands(commands)
+
+cursor.execute("SELECT COUNT(*) FROM rescind_counter WHERE name='Steve Jobs#9339';")
+result = cursor.fetchone()[0]
+
+cursor.execute("SELECT COUNT(*) FROM rescind_counter WHERE name='John Doe';")
+result2 = cursor.fetchone()[0]
+
+print(f'count for jobs: {result}\ncount for john doe: {result2}')
+
+conn.close()
