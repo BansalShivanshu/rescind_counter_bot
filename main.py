769494c7c793e765d2f47c686e2c94ff4c83a02e
Shivanshu Bansal <sdev.shivanshu@gmail.com>
2023-04-21 04:07:14 -0700

    template code done

--- main.py
+++ main.py
@@ -8,5 +8,20 @@ bot = commands.Bot('^', token = TOKEN)
 async def check_for_rescind(message):
     # check if the message contains rescind
     if 'rescind' in message.content.lower():
-        
+        # get the user data -- username     count
+        # increment
+        # save
         pass
+
+# define command behaviour
+@bot.command
+async def count(ctx, user: commands.MemberConverter):
+    # Get the user's 'rescind' counter from a database or file
+    # Send a message to the channel with the user's counter
+    # await ctx.send(f"{user.mention}, your rescind count is {counter}.")
+
+# listen for incoming messages
+@bot.event
+async def on_message(message):
+    await check_for_rescind(message)
+    await bot.process_commands(commands)
